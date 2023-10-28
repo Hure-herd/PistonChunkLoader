607a3bb9ba2322444be6866d14e09812c562a0d0
紫薯薯薯薯桑 <1464234368q@gmail.com>
2023-10-29 01:34:42 +0800

    1

---
+++ src/main/java/rems/mod/Tickets/PistonBlockTickets.java
@@ -0,0 +1,9 @@
+package rems.mod.Tickets;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PistonBlockTickets implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/rems/mod/Tickets/PistonChunkUtility.java
@@ -0,0 +1,14 @@
+package rems.mod.Tickets;
+
+import net.minecraft.server.world.ChunkTicketType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ChunkPos;
+
+import java.util.Comparator;
+
+public class PistonChunkUtility
+{
+    public static final ChunkTicketType<ChunkPos> PISTON_BLOCK_TICKET = ChunkTicketType.create("piston_block", Comparator.comparingLong(ChunkPos::toLong), 8);
+
+    public static final int obsidianHash = new Identifier("minecraft", "obsidian").hashCode();
+}
